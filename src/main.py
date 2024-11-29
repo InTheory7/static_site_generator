0ee60194d757026f0430cfd80217e12a35955bdd
InTheory7 <joshuacollins999@gmail.com>
2024-11-29 08:37:16 +0000

    Created the TextNode and printed its string representation in main()

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+from textnode import TextNode
+
+def main():
+    # Create an example:
+    example_textNode = TextNode("Here's some text", "bold", "https://www.boot.dev")
+    # Print the example:
+    print(example_textNode)
+
+main()
