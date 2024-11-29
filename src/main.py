99f24dbfdded6fa506349fd7576d391ecc629385
InTheory7 <joshuacollins999@gmail.com>
2024-11-29 09:35:46 +0000

    Added comments

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode
 
 def main():
-    # Create an example:
+    # Create an example TextNode:
     example_textNode = TextNode("Here's some text", "bold", "https://www.boot.dev")
     # Print the example:
     print(example_textNode)
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+# TextType enum which is used in the TextNode class
 class TextType(Enum):
     NORMAL = "normal"
     BOLD = "bold"
@@ -8,17 +9,22 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
+# TextNode class which contains information about inline text
 class TextNode:
     def __init__(self, text, text_type, url=None):
-        self.text = text
-        self.text_type = TextType(text_type)
-        self.url = url
+        self.text = text # The actual body of the text itself
+        self.text_type = TextType(text_type) # What format (TextType enum) it should be in
+        self.url = url # A link for regular links or images
 
+    # Check that two TextNodes are equal by seeing if all of their properties are equal
+    # (this might not work yet)
     def __eq__(self, other_TextNode):
         if self == other_TextNode:
             return True
         else:
             return False
-        
+    
+    # Create the string representation of this TextNode class, so it displays correctly
+    # when print() is used elsewhere
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
