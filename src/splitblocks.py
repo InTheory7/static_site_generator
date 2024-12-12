2de89ede3feeea38a40cd9565dd26303530528ce
InTheory7 <joshuacollins999@gmail.com>
2024-12-12 00:37:36 +0000

    Added a function to detect the markdown block type and completed unit tests

--- src/splitblocks.py
+++ src/splitblocks.py
@@ -2,9 +2,81 @@ def markdown_to_blocks(markdown):
     # Make sure the input is a string:
     if not isinstance(markdown, str):
         raise Exception("Input must be a string")
-    # Split the markdown input at newlines:
-    blocks = markdown.split("\n")
+    # Split the markdown input at double newlines:
+    blocks = markdown.split("\n\n")
     # Remove whitespace from each block (without a for loop):
     new_blocks = list(map(lambda block: block.strip(), blocks))
     # Remove empty strings from the list (without a for loop):
-    return list(filter(lambda block: block != "", new_blocks))
+    return list(filter(lambda block: block != ("" or "\n"), new_blocks))
+
+from itertools import groupby
+
+def block_to_block_type(block):
+    # Make sure the input is a string:
+    if not isinstance(block, str):
+        raise Exception("Input must be a string")
+    # Make sure the string isn't empty:
+    if block.strip() == "":
+        return "normal"
+    
+    # Check if the block is a heading:
+    groups = []
+    for _, group in groupby(block):
+        groups.append(list(group))
+    if (block[0] == "#") and (
+            (len(groups[0]) >= 1) and (len(groups[0]) <= 6)
+        ):
+        return "heading"
+
+    # Check if the block is code:
+    if len(block) >= 6:
+        if block[:3] == "```" and block[-3:] == "```":
+            return "code"
+
+    # Split the block at newlines:
+    lines = block.split("\n")
+    # Remove whitespace from each line (without a for loop):
+    new_lines = list(map(lambda line: line.strip(), lines))
+    print(new_lines)
+
+    # Check if the block is a quote, unordered, or ordered list
+    quote_true = True
+    unordered_true = True
+    ordered_true = True
+    order_count = 0
+    for line in new_lines:
+        # Ignore empty strings in the list:
+        if len(line) == 0:
+            continue
+        # Quote check:
+        if line[0] != ">":
+            quote_true = False
+        # Unordered check:
+        if len(line) > 2:
+            bullet_point = False
+            dash_point = False
+            if line[:2] == "* ":
+                bullet_point = True
+            elif line[:2] == "- ":
+                dash_point = True
+            if (not bullet_point) and (not dash_point):
+                unordered_true = False
+        else:
+            unordered_true = False
+        # Ordered check:
+        order_count += 1
+        if len(line) > (int(order_count/10) + 1 + 2):
+            if line[:(int(order_count/10) + 1 + 2)] != f"{order_count}. ":
+                ordered_true = False
+        else:
+            ordered_true = False
+
+    if quote_true:
+        return "quote"
+    if unordered_true:
+        return "unordered_list"
+    if ordered_true:
+        return "ordered_list"
+    
+    # Otherwise it's a paragraph
+    return "normal"
--- src/test_splitblocks.py
+++ src/test_splitblocks.py
@@ -1,5 +1,5 @@
 import unittest
-from splitblocks import markdown_to_blocks
+from splitblocks import markdown_to_blocks, block_to_block_type
 
 class TestMarkdownToBlocks(unittest.TestCase):
     # Pass in an empty string:
@@ -38,6 +38,123 @@ class TestMarkdownToBlocks(unittest.TestCase):
         * This is a list item
         * This is another list item"""
         print(markdown_to_blocks(text))
+
+class TestBlockToBlockType(unittest.TestCase):
+    # Pass in an empty string:
+    def test_01emptyString(self):
+        text = ""
+        print(f"block_to_block_type test results:\nWant (normal): {block_to_block_type(text)}")
+
+    # Pass in a list:
+    ### def test_02list(self):
+    ###     text = ["String in a list"]
+    ###     print(block_to_block_type(text))
+    # ^ Test failed as it should.
+
+    # Pass in a single line of text:
+    def test_03plainText(self):
+        text = "Here's some text"
+        print(f"Want (normal): {block_to_block_type(text)}")
+
+    # Pass in a heading:
+    def test_04heading(self):
+        text = "# Heading"
+        print(f"Want (heading): {block_to_block_type(text)}")
+
+    # Pass in a different kind of heading:
+    def test_05heading2(self):
+        text = "#### Heading"
+        print(f"Want (heading): {block_to_block_type(text)}")
+
+    # Pass in an invalid heading:
+    def test_06invalidHeading(self):
+        text = "############# Heading"
+        print(f"Want (normal): {block_to_block_type(text)}")
+
+    # Pass in a code block:
+    def test_07codeBlock(self):
+        text = "```Here's some code```"
+        print(f"Want (code): {block_to_block_type(text)}")
+
+    # Pass in an invalid code block:
+    def test_08invalidCodeBlock(self):
+        text = "```Here's some wrong code``"
+        print(f"Want (normal): {block_to_block_type(text)}")
+
+    # Pass in a quote:
+    def test_09quote(self):
+        text = ">Quote"
+        print(f"Want (quote): {block_to_block_type(text)}")
+
+    # Pass in a multi-line quote:
+    def test_10multilineQuote(self):
+        text = """>Here's the start of the quote
+        > here's some more
+        > and here's the end.
+        """
+        print(f"Want (quote): {block_to_block_type(text)}")
+
+    # Pass in an unordered list:
+    def test_11unorderedList(self):
+        text = """* Here's the start of a list.
+        * Here's the end.
+        """
+        print(f"Want (unordered_list): {block_to_block_type(text)}")
+
+    # Pass in an unordered list:
+    def test_12unorderedList(self):
+        text = """- Here's the start of a list.
+        - Here's the end.
+        """
+        print(f"Want (unordered_list): {block_to_block_type(text)}")
+
+    # Pass in an unordered list:
+    def test_13unorderedList(self):
+        text = """* Here's the start of a list.
+        - Here's the middle.
+        * Here's the end.
+        """
+        print(f"Want (unordered_list): {block_to_block_type(text)}")
+
+    # Pass in an incorrect unordered list:
+    def test_14invalidUnorderedList(self):
+        text = """* Here's the start of a list.
+         Here's the middle.
+        * Here's the end.
+        """
+        print(f"Want (normal): {block_to_block_type(text)}")
+
+    # Pass in an ordered list:
+    def test_15orderedList(self):
+        text = """1. Here's the start
+        2. Here's the middle
+        3. Here's the end.
+        """
+        print(f"Want (ordered_list): {block_to_block_type(text)}")
+
+    # Pass in an ordered list with incorrect numbers:
+    def test_16invalidOrderedList(self):
+        text = """1. Here's the start
+        3. Here's the middle
+        1. Here's the end.
+        """
+        print(f"Want (normal): {block_to_block_type(text)}")
+
+    # Pass in a long ordered list:
+    def test_17orderedList(self):
+        text = """1. Here's the start
+        2. Here's the middle
+        3. Here's the end.
+        4. 4
+        5. 5
+        6. 6
+        7. 7
+        8. 8
+        9. 9
+        10. 10
+        11. 11
+        """
+        print(f"Want (ordered_list): {block_to_block_type(text)}")
 
 if __name__ == "__main__":
     unittest.main()
